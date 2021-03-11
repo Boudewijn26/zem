1d80d101ca272d83c8ddd0f63d2c0137c9e08e1e
Miriam <miriam@contentisbv.nl>
2021-03-11 14:21:36 +0100

    Support for package - step 1

--- tools/quicktype/src/generate_source.js
+++ tools/quicktype/src/generate_source.js
@@ -1,121 +1,163 @@
-const path = require('path'); 
-const { promises: { 
-	readFile,
-	readdir,
-	writeFile
-} } = require('fs');
+/**
+ * Generates api code for different languages from json schemas
+ */
+const path = require("path");
+const {
+  promises: { readFile, readdir, writeFile },
+} = require("fs");
 
 const {
-	quicktype,
-	InputData,
-	jsonInputForTargetLanguage,
-	quicktypeMultiFile,
+  quicktype,
+  InputData,
+  jsonInputForTargetLanguage,
+  quicktypeMultiFile,
 } = require("quicktype-core");
 
 // Config
 const targetLanguage = "Java";
 const basePath = "../../../specs/models/schema/";
+const baseCodePath = "../../../generated-sources/models";
 
 /**
  * MAIN FUNCTION
- * 
- * @returns 
+ *
+ * @returns
  */
 async function main() {
+  //
+  // TODO scan schema folder
+  // Do we want to scan subfolders?
+  //
 
-	//
-	// TODO scan schema folder
-	// Do we want to scan subfolders?
-	//
+  createFolderIfNotExist(baseCodePath);
+  createFolderIfNotExist(path.join(baseCodePath, targetLanguage));
+  createFolderIfNotExist(path.join(baseCodePath, "Python"));
 
-	const testFolder = basePath + '/shower';
-	const inputData = new InputData();
-
-	const files = await readdir(testFolder);
-
-    await addJsonFilesToSchema(inputData, files, testFolder);
-
-	//
-	// TODO Change package with subdir?
-	// for ex:
-	// schema/shower/*.json becomes
-	// nl.contentisbv.zem.models.shower
-	// 
-	const result =  await quicktypeMultiFile({
-		inputData,
-		lang: targetLanguage,
-		rendererOptions: { package: "nl.contentisbv.zem.models" }
-	});
-
-	const writes = Array.from(result).map(async ([filename, result]) => {
-
-		// 
-		// TODO change temp to another directory
-		// Proposal: <root>/generated_resources
-		//
-		await writeFile(path.join("./temp", filename), result.lines.join("\n"), "utf-8");
-	});
-	await Promise.all(writes);
-
-	return result;
+  return await generateApi("shower");
 }
 
 main();
-//main().then((result) => console.log(result.lines.join("\n")));
+
+/**
+ * Generate an api for all json files in this subfolder.
+ *
+ * Package name will be appended with subfolder name
+ *
+ * @param {*} subPath
+ * @returns
+ */
+async function generateApi(subPath) {
+  const outputFolder = path.join(
+    path.join(baseCodePath, targetLanguage),
+    subPath
+  );
+  const inputFolder = basePath + "/" + subPath;
+
+  createFolderIfNotExist(outputFolder);
+
+  const inputData = new InputData();
+
+  const files = await readdir(inputFolder);
+
+  await addJsonFilesToSchema(inputData, files, inputFolder);
+
+  //
+  // TODO Change package with subdir?
+  // for ex:
+  // schema/shower/*.json becomes
+  // nl.contentisbv.zem.models.shower
+  //
+  const result = await quicktypeMultiFile({
+    inputData,
+    lang: targetLanguage,
+    rendererOptions: { package: "nl.contentisbv.zem.models." + subPath },
+  });
+
+  const writes = Array.from(result).map(async ([filename, result]) => {
+    //
+    // TODO change temp to another directory
+    // Proposal: <root>/generated_resources
+    //
+
+    //
+    // TODO delete old files.
+    //
+    // Python support -> stdout naar filename (use directory name)
+    await writeFile(
+      path.join(outputFolder, filename),
+      result.lines.join("\n"),
+      "utf-8"
+    );
+  });
+  await Promise.all(writes);
+
+  return result;
+}
 
 //-----------------------------------------------------------------------------------------------------------
 
 /**
  * Scans directory for json files and adds them to the schema.
- * 
- * @param {*} inputData 
- * @param {*} files 
- * @param {*} testFolder 
+ *
+ * @param {*} inputData
+ * @param {*} files
+ * @param {*} testFolder
  */
 async function addJsonFilesToSchema(inputData, files, testFolder) {
-	
-	console.log("addJsonFilesToSchema");
+  console.log("addJsonFilesToSchema");
 
-	const promises = files.filter(jsonExtensionFilter(".json")).map(async function (name) {
+  const promises = files
+    .filter(jsonExtensionFilter(".json"))
+    .map(async function (name) {
+      await addFileToInputSchema(inputData, testFolder, name);
+    });
 
-		await addFileToInputSchema(inputData, testFolder, name);
-
-	});
-
-	await Promise.all(promises);
+  await Promise.all(promises);
 }
 
 /**
  * Adds json files to the quicktype source
- * 
- * @param {*} inputData 
- * @param {*} testFolder 
- * @param {*} name 
+ *
+ * @param {*} inputData
+ * @param {*} testFolder
+ * @param {*} name
  */
-async function addFileToInputSchema( inputData, testFolder, name) {
+async function addFileToInputSchema(inputData, testFolder, name) {
+  console.log("Adding source " + name + " to Quicktype source ");
 
-	console.log("Adding source " + name + "to Quicktype source ");
+  const filePath = path.join(testFolder, name);
+  const result = path.parse(filePath);
 
-	const filePath = path.join(testFolder, name);
-	const result = path.parse(filePath);
+  const objectName = result.name;
 
-	const objectName = result.name;
+  const schemaData = await readFile(filePath, "utf8");
 
-	
-	const schemaData = await readFile(filePath, 'utf8');
-	
-	const input = jsonInputForTargetLanguage(targetLanguage);
-	await input.addSource({ name: objectName, samples: [schemaData] });
-	
-	inputData.addInput(input);
+  const input = jsonInputForTargetLanguage(targetLanguage);
+  await input.addSource({ name: objectName, samples: [schemaData] });
+
+  inputData.addInput(input);
 }
 
 /**
  * Creates a filter for json
- * 
- * @param {*} extension 
- * @returns 
+ *
+ * @param {*} extension
+ * @returns
  */
 function jsonExtensionFilter(extension) {
-	return (element) => path.extname(element) === extension; 
+  return (element) => path.extname(element) === extension;
+}
+
+/**
+ * Checks if a folder exists, if not it will be created
+ *
+ * @param {*} folder
+ */
+function createFolderIfNotExist(folder) {
+  const fs = require("fs");
+
+  if (!fs.existsSync(folder)) {
+    console.log("Creating folder " + folder);
+    fs.mkdirSync(folder);
+  }
 }
