8ecc0e78b4ea22478f37800175ec338a5fcade69
Boudewijn van Groos <B.vanGroos@sping.nl>
2021-03-12 10:40:18 +0100

    Package Java generated code into gradle with example

---
+++ generated-sources/models/java/src/main/java/nl/contentisbv/zem/models/coffee/Coffee.java
@@ -0,0 +1,12 @@
+package nl.contentisbv.zem.models.coffee;
+
+import com.fasterxml.jackson.annotation.*;
+
+public class Coffee {
+    private Usage usage;
+
+    @JsonProperty("usage")
+    public Usage getUsage() { return usage; }
+    @JsonProperty("usage")
+    public void setUsage(Usage value) { this.usage = value; }
+}
---
+++ generated-sources/models/java/src/main/java/nl/contentisbv/zem/models/shower/PredictedUsage.java
@@ -0,0 +1,15 @@
+package nl.contentisbv.zem.models.shower;
+
+import com.fasterxml.jackson.annotation.*;
+
+/**
+ * The predicted usage of the shower
+ */
+public class PredictedUsage {
+    private UsageElement[] usages;
+
+    @JsonProperty("usages")
+    public UsageElement[] getUsages() { return usages; }
+    @JsonProperty("usages")
+    public void setUsages(UsageElement[] value) { this.usages = value; }
+}
