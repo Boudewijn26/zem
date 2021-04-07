de6d68593e7c24c9d78d0475445eab893c6c9f74
Kees Jan Koster <kjkoster@kjkoster.org>
2021-04-07 16:00:50 +0200

    Fix the admin token.

--- tools/influx-cli/influx.sh
+++ tools/influx-cli/influx.sh
@@ -2,4 +2,4 @@
 #
 # A quick wrapper to be able to talk to a remote Influx database.
 #
-docker run -it influxdb /usr/local/bin/influx --host http://35.190.206.110:8086 --token azYwfXHXauDAb0ZW2GWLM4PkZQjox6e9z1hUGXEEE-zxzMWFr9LAW3Csj2aMiVGAh1HFnk8vpY78s4ekhJZ2nw== ${*}
+docker run -it influxdb /usr/local/bin/influx --host http://35.190.206.110:8086 --token FCl28DwmhZF1lVp3aRfJnN5u-S7gw-b114MpONSmJ1jbaNhwEjepFy0mxg01WEpHDnyhEjalqJsOBVssSDkiqg== ${*}
